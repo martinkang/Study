243634e5a64630c78403c01e9728a21228943f8f
martinkang <hiyou63@naver.com>
2016-08-31 00:08:46 +0900

    abstractfactory

--- DesignPatterns/Creational_Patterns/AbstractFactory.cpp
+++ DesignPatterns/Creational_Patterns/AbstractFactory.cpp
@@ -1,5 +1,18 @@
 #include "maze.h"
 
+enum Spell
+{
+	None = -1,
+	Magic = 0,
+};
+
+enum MazeType
+{
+	Normal = 0,
+	Enchanted = 1,
+	Bomb = 2
+};
+
 class MazeFactory
 {
 public:
@@ -12,7 +25,7 @@ public:
 
 	virtual Wall * MakeWall() const
 	{
-		return New Wall();
+		return new Wall();
 	}
 
 	virtual Room * MakeRoom( int aRoomNo ) const
@@ -39,48 +52,133 @@ Maze * MazeGame::CreateMaze( MazeFactory & aFactory )
 	sMaze->AddRoom( sR1 );
 	sMaze->AddRoom( sR2 );
 
-	sR1->SetSide( North, aFactory.Wall() );
+	sR1->SetSide( North, aFactory.MakeWall() );
 	sR1->SetSide( East, sDoor );
-	sR1->SetSide( South, aFactory.Wall() );
-	sR1->SetSide( West, aFactory.Wall() );
+	sR1->SetSide( South, aFactory.MakeWall() );
+	sR1->SetSide( West, aFactory.MakeWall() );
 
-	sR2->SetSide( North, aFactory.Wall() );
-	sR2->SetSide( East, aFactory.Wall() );
-	sR2->SetSide( South, aFactory.Wall() );
+	sR2->SetSide( North, aFactory.MakeWall() );
+	sR2->SetSide( East, aFactory.MakeWall() );
+	sR2->SetSide( South, aFactory.MakeWall() );
 	sR2->SetSide( West, sDoor );
 
 	return sMaze;
-}
+};
 
+class EnchantedRoom : public Room
+{
+public:
+	EnchantedRoom( int aRoomNo, Spell aSpell ) : Room( aRoomNo )
+	{
+		_spell = aSpell;
+	}
+
+	virtual void Enter()
+	{
+		cout << "Magic Room!!!!" << endl;
+	}
+
+private:
+	Spell _spell;
+};
+
+class DoorNeedingSpell : public Door
+{
+public:
+	DoorNeedingSpell( Room * aR1, Room * aR2 ) : Door( aR1, aR2 )
+	{
+		_spell = Magic;
+	}
+	virtual void Enter()
+	{
+		cout << " Magic Door!!!!" << endl;
+	}
+
+private:
+	Spell _spell;
+};
 
 /* EnchantedMazeFactory 를 MazeFactory 에서 서브클래싱한 후, 멤버 함수를 재정의하여 
    Room, Wall 을 상속하는 다른 서브클래스의 인스턴스를 반환하게 만든다. */
-class EnchanteMazeFactory : public MazeFactory
+class EnchantedMazeFactory : public MazeFactory
 {
 public:
 	EnchantedMazeFactory();
-
 	/* 이 때 Room 을 상속받은 EnchantedRoom 의 인스턴스를 생성하여 반환함 */
-	virtual Room * MakeRoom( int n ) const
+	virtual Room * MakeRoom( int aRoomNo ) const
 	{
-		return new EnchantedRoom( n, CastSpell() );
+		return new EnchantedRoom( aRoomNo, CastSpell() );
 	}
-
 	/* 이 때 Door 을 상속받은 DoorNeedingSpell 의 인스턴스를 생성하여 반환함 */
-	virtual Door * MakeDoor( Room * r1, Room * r2 ) const
+	virtual Door * MakeDoor( Room * aR1, Room * aR2 ) const
 	{
-		return new DoorNeedingSpell( r1, r2 );
+		return new DoorNeedingSpell( aR1, aR2 );
 	}
 
 protected:
-	Spell * CastSpell() const;
-}
+	Spell CastSpell() const
+	{
+		return Magic;
+	}
+};
+
+class RoomWithABomb : public Room
+{
+public:
+	RoomWithABomb( int aRoomNo ) : Room( aRoomNo ) { }
+
+	virtual void Enter()
+	{
+		cout << "Bomb!!!!" << endl;
+	}
+private:
+};
+
+class BombedWall : public Wall
+{
+public:
+	BombedWall() : Wall() {}
+	
+	virtual void Enter()
+	{
+		cout << "Bomb!!!!" << endl;
+	}
+};
+
+class BombedMazeFactory : public MazeFactory
+{
+public:
+	BombedMazeFactory();
+
+	virtual Room * MakeRoom( int aRoomNo ) const
+	{
+		return new RoomWithABomb( aRoomNo );
+	}
+
+	virtual Wall * MakeWall() const
+	{
+		return new BombedWall();
+	}
+
+private:
+};
 
 
 int main( void )
 {
-	Maze *sMaze = NULL;
-	MazeGame * sMazeGame = new MazeGame();
-	
-	sMaze = sMazeGame->CreateMaze();	
+	Maze * sMaze1 = NULL;
+	Maze * sMaze2 = NULL;
+	Maze * sMaze3 = NULL;
+
+	MazeGame sGame;
+
+	MazeFactory sN_Factory();
+	EnchantedMazeFactory sE_Factory();
+	BombedMazeFactory sB_Factory();
+
+	sMaze1 = sGame.CreateMaze( sN_Factory );
+/*	sMaze2 = sGame.CreateMaze( sE_Factory );
+	sMaze3 = sGame.CreateMaze( sB_Factory );
+*/
+	return 0;
 }
